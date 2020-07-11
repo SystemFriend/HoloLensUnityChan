949ab7045692d018813587e4250d3ee587e43c10
satoshi-maemoto <peugeot.106.s16@gmail.com>
2020-07-12 01:12:52 +0900

    Add Unity-chan SSU

--- Assets/Application/Scripts/Control.cs
+++ Assets/Application/Scripts/Control.cs
@@ -10,6 +10,7 @@ public class Control : MonoBehaviour
 {
     public TextMesh guidanceText;
     public GameObject unityChan;
+    public GameObject unityChan2;
     public AudioSource audioSource;
     public GameObject floor;
     private IList<GameObject> copyActors = new List<GameObject>();
@@ -41,9 +42,12 @@ public class Control : MonoBehaviour
     {
         var startTime = DateTime.Now;
         var unityChanRigidbody = this.unityChan.GetComponent<Rigidbody>();
+        var unityChanRigidbody2 = this.unityChan2.GetComponent<Rigidbody>();
         var initialPosition = this.unityChan.transform.localPosition;
+        var initialPosition2 = this.unityChan.transform.localPosition;
         this.copyActors = new List<GameObject>();
 
+        unityChanRigidbody.isKinematic = true;
         unityChanRigidbody.isKinematic = true;
         while (true)
         {
@@ -51,6 +55,7 @@ public class Control : MonoBehaviour
             {
                 //10秒後にisKinematicを開放して重力落下を開始させる
                 unityChanRigidbody.isKinematic = false;
+                unityChanRigidbody2.isKinematic = false;
             }
             else if (unityChanRigidbody.isKinematic)
             {
@@ -65,6 +70,9 @@ public class Control : MonoBehaviour
                 this.unityChan.transform.localPosition = initialPosition;
                 this.unityChan.SetActive(true);
 
+                this.unityChan2.SetActive(false);
+                this.unityChan2.transform.localPosition = initialPosition2;
+                this.unityChan2.SetActive(true);
                 foreach (var copy in this.copyActors)
                 {
                     GameObject.Destroy(copy);
@@ -80,6 +88,11 @@ public class Control : MonoBehaviour
                 //Unityちゃん落下消失対策
                 this.unityChan.transform.localPosition = initialPosition;
             }
+            if (Vector3.Distance(this.unityChan2.transform.position, Vector3.zero) > 20.0f)
+            {
+                //Unityちゃん落下消失対策
+                this.unityChan2.transform.localPosition = initialPosition2;
+            }
             this.guidanceText.text = string.Empty;
 
             yield return 0;
@@ -90,7 +103,7 @@ public class Control : MonoBehaviour
     {
         var animHash = this.unityChan.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).fullPathHash;
         var animTime = this.unityChan.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime;
-        var copy = GameObject.Instantiate(this.unityChan);
+        var copy = GameObject.Instantiate(((this.copyActors.Count % 2) == 0) ? this.unityChan : this.unityChan2);
         copy.GetComponent<Animator>().Play(animHash, -1, animTime);
 
         copy.transform.position = pointer.Result.Details.Point;
